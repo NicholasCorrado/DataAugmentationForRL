270be165c285f2c7a395f6f2367c9beb5f2998e4
Nora Tseng <nora@tseng.us>
2024-04-12 00:07:29 -0700

    added network structs

--- src/ddpg.py
+++ src/ddpg.py
@@ -112,21 +112,6 @@ def make_env(env_id, env_kwargs, seed, idx, capture_video, run_name):
 
     return thunk
 
-# ALGO LOGIC: initialize agent here:
-# class QNetwork(nn.Module):
-#     def __init__(self, env):
-#         super().__init__()
-#         self.fc1 = nn.Linear(np.array(env.single_observation_space.shape).prod() + np.prod(env.single_action_space.shape), 64)
-#         self.fc2 = nn.Linear(64,64)
-#         self.fc3 = nn.Linear(64, 1)
-
-#     def forward(self, x, a):
-#         x = torch.cat([x, a], 1)
-#         x = F.relu(self.fc1(x))
-#         x = F.relu(self.fc2(x))
-#         x = self.fc3(x)
-#         return x
-
 # Uses param network_struct to construct network
 class QNetwork(nn.Module):
     def __init__(self, env):
