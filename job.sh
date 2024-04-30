5bcd5eef583795fbca229a6dfcfc2bc4d6b26a9a
Nicholas Corrado <nicholascorrado@Nicholass-MacBook-Air-2.local>
2024-04-30 16:28:17 -0500

    use conda env

--- job.sh
+++ job.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
 
-# untar python installation and make sure the script uses it
-tar -xzf python39.tar.gz
-export PATH=$PWD/python/bin:$PATH
-rm python39.tar.gz
-
-# fetch your packages from /staging/
-cp /staging/ntseng/packages.tar.gz .
-tar -xzf packages.tar.gz
-rm packages.tar.gz
-# make sure python knows where your packages are
-export PYTHONPATH=$PWD/packages
-
-# fetch your code from /staging/
-CODENAME=DataAugmentationForRL              ########## change when switching repos
-cp /staging/ntseng/${CODENAME}.tar.gz .
-tar -xzf ${CODENAME}.tar.gz
-rm ${CODENAME}.tar.gz
-
-python3 -m pip install --upgrade pip
-cd $CODENAME
-python3 -m pip install -e .
-cd src
-python3 -m pip install -e custom-envs
-
+# for debugging
 pid=$1
 step=$2 #ranges from 0 to num_jobs-1
 cmd=`tr '*' ' ' <<< $3` # replace * with space
 echo $cmd $pid $step
 
-# run your script
-# $step ensures seeding is constistent across experiment batches
-$($cmd --run_id $pid --seed $step)
+# fetch your code from /staging/
+CODENAME=DataAugmentationForRL
+cp /staging/ncorrado/${CODENAME}.tar.gz .
+tar -xzf ${CODENAME}.tar.gz
+rm ${CODENAME}.tar.gz
+
+# fetch conda env from stathing and activate it
+ENVNAME=da4rl
+ENVDIR=${ENVNAME}
+cp /staging/ncorrado/${ENVNAME}.tar.gz .
+mkdir $ENVDIR
+tar -xzf ${ENVNAME}.tar.gz -C $ENVDIR
+rm ${ENVNAME}.tar.gz # remove env tarball
+source $ENVDIR/bin/activate
+
+# install editable packages (editable packages cannot be packaged in the env tarball)
+cd $CODENAME
+pip install -e .
+pip install -e custom-envs
+cd src
+
+# run your script -- $step ensures seeding is consistent across experiment batches
+$($cmd --run_id $step --seed $step)
 
 # compress results. This file will be transferred to your submit node upon job completion.
 tar czvf results_${pid}.tar.gz results
