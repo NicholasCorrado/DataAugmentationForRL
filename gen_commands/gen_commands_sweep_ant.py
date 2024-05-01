fa746c5d23c3d01b06e609c5b99e38f45546e188
nytseng <nora@tseng.us>
2024-05-01 18:47:33 -0500

    set aug_ratio default 1, added more gen_commands

--- gen_commands/gen_commands_sweep_ant.py
+++ gen_commands/gen_commands_sweep_ant.py
@@ -49,7 +49,25 @@ def gen_command_net(env_id, learning_rate, batch_size, network, network_subdir):
                     f'--learning_rate {learning_rate} ' \
                     f'--batch_size {batch_size} ' \
                     f'--net_arch {network} ' \
-                    f'--save_subdir lr_{learning_rate}_batch_{batch_size}/network_{network_subdir} ' \
+                    f'--save_subdir lr_{learning_rate}/tau_ ' \
+                    f'--env_kwargs continuing_task False ' \
+                    f'--total_timesteps 1000000 ' \
+                    f'--buffer_size {int(1e6)}'
+
+    mem = 10
+    disk = 10
+    command = f"{mem},{disk},{python_command}"
+    return command
+
+# 4/28
+def gen_command_tau(env_id, learning_rate, tau):
+    python_command = f'python3 ddpg.py --env_id {env_id} ' \
+                    f'--random_action_prob 0 ' \
+                    f'--learning_starts 10000 ' \
+                    f'--learning_rate {learning_rate} ' \
+                    f'--batch_size 64 ' \
+                    f'--net_arch 256 256 256 ' \
+                    f'--save_subdir lr_{learning_rate}/tau_{tau} ' \
                     f'--env_kwargs continuing_task False ' \
                     f'--total_timesteps 1000000 ' \
                     f'--buffer_size {int(1e6)}'
@@ -64,12 +82,11 @@ if __name__ == "__main__":
     os.makedirs('commands', exist_ok=True)
     f = open(f"commands/train_ant_sweep.txt", "w")
     
-    # env_ids = ['PointMaze_UMaze-v3', 'PointMaze_Medium-v3', 'PointMaze_Large-v3']
     # pointmaze_dafs = ['RelabelGoal', 'TranslateRotate', 'TranslateRotateRelabelGoal']
-    learning_rates = [1e-3, 1e-4]
     batch_sizes = [64, 128, 256]
 
-    env_ids = ['AntMaze_UMaze-v3', 'AntMaze_BigMaze-v3', 'AntMaze_HardestMaze-v3 ']
+    # env_ids = ['AntMaze_UMaze-v4', 'AntMaze_Medium-v4', 'AntMaze_Large-v4']
+    env_ids = ['AntMaze_UMaze-v4', 'AntMaze_UMazeDense-v4']
     # pointmaze_dafs = ['RelabelGoal']
     # buff = 2e6
     # aug_ratios = [1,2,4,8,16]
@@ -79,18 +96,28 @@ if __name__ == "__main__":
     net_arch_str = ["64,64", "256,256", "256,256,256"]
 
     # sweep over batch size with daf RelabelGoal
+    # for env_id in env_ids: 
+    #     for lr  in learning_rates:
+    #         for batch in batch_sizes:
+    #             command = gen_command_batch(env_id, daf=None, learning_rate=lr, batch_size=batch)
+    #             print(command)
+    #             f.write(command.replace(' ', '*') + "\n")
+
+    #             for network, subdir in zip(net_archs,net_arch_str):
+    #                 command = gen_command_net(env_id, lr, batch, network, subdir)
+    #                 print(command)
+    #                 f.write(command.replace(' ', '*') + "\n")
+
+    # lr + tau sweep
+    learning_rates = [1e-4, 1e-5]
+    taus= [0.05, 0.005]
+    env_ids = ['AntMaze_UMaze-v4', 'AntMaze_UMazeDense-v4']
 
     for env_id in env_ids: 
         for lr  in learning_rates:
-            for batch in batch_sizes:
-                command = gen_command_batch(env_id, daf=None, learning_rate=lr, batch_size=batch)
+            for tau in taus:
+                command = gen_command_tau(env_id, lr, tau)
                 print(command)
                 f.write(command.replace(' ', '*') + "\n")
-
-                for network, subdir in zip(net_archs,net_arch_str):
-                    command = gen_command_net(env_id, lr, batch, network, subdir)
-                    print(command)
-                    f.write(command.replace(' ', '*') + "\n")
-
 
     
