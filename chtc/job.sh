5efbc73e52e676a6fe7f6b91ce42b8a388aeea05
ncorrado <ncorrado@ap2001.chtc.wisc.edu>
2024-05-29 21:19:37 -0500

    Silly docker hack

--- chtc/job.sh
+++ chtc/job.sh
@@ -13,19 +13,24 @@ rm ${CODENAME}.tar.gz
 echo $CODENAME
 
 # fetch conda env from stathing and activate it
-ENVNAME=da4rl
-ENVDIR=${ENVNAME}
-cp /staging/ncorrado/${ENVNAME}.tar.gz .
-mkdir $ENVDIR
-tar -xzf ${ENVNAME}.tar.gz -C $ENVDIR
-rm ${ENVNAME}.tar.gz # remove env tarball
-source $ENVDIR/bin/activate
-conda list
+#ENVNAME=da4rl
+#ENVDIR=${ENVNAME}
+#cp /staging/ncorrado/${ENVNAME}.tar.gz .
+#mkdir $ENVDIR
+#tar -xzf ${ENVNAME}.tar.gz -C $ENVDIR
+#rm ${ENVNAME}.tar.gz # remove env tarball
+#source $ENVDIR/bin/activate
+#conda list
+
+python3 -m venv env
+source env/bin/activate
 
 # install editable packages (editable packages cannot be packaged in the env tarball)
 cd $CODENAME
 pip install -e .
 pip install -e src/custom-envs
+pip install torch --index-url https://download.pytorch.org/whl/cpu
+pip install gymnasium panda-gym tyro pyyaml stable-baselines3 tensorboard gymnasium_robotics rliable
 cd src
 
 # run your script -- $step ensures seeding is consistent across experiment batches
